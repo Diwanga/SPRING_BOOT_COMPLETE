e27e28f2468a547d41de5b99a81afda00c2f6466
Diwanga Amasith <diwangaamasith@Diwangas-MacBook-Pro.local>
2022-04-17 09:06:52 +0530

    done

--- SpringFrameworkDemo/src/main/java/demo/Doctor.java
+++ SpringFrameworkDemo/src/main/java/demo/Doctor.java
@@ -1,16 +1,27 @@
 package demo;
 
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.sql.SQLOutput;
 
 
-public class Doctor implements Staff {
+public class Doctor implements Staff , BeanNameAware {
 
     private String university;
     private Nurse nurse;
     public void assist(){
         System.out.println("assisting by 454doctor");
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("Aware interface"); // SEE THIS ----->>>>> https://www.baeldung.com/spring-bean-name-factory-aware
+    }
+@PostConstruct
+    public void postConstruct(){
+        System.out.println("post construct");
     }
 
 //    public void setNurse(Nurse nurse) {
